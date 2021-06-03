089f09d39ef48807efd591f4f39af2c4e68cc775
Joshua Abeyta <mentaltemple@gmail.com>
2021-06-03 15:44:10 -0600

    built out api routes

--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,7 @@ const session = require("express-session");
 
 const withAuth = (req, res, next) => {
   // If the user hasn't logged in, this will redirect them to login
-  if (!req.session.loggedIn) {
+  if (!req.session.logged_in) {
     res.redirect("/login");
   } else {
     next();
