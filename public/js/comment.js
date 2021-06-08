55dfc2e444137026cefdfe0e5b0dbb75ff0bcfe3
Joshua Abeyta <mentaltemple@gmail.com>
2021-06-08 16:04:10 -0600

    added comment js route

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,5 +1,21 @@
-commentFormSubmit = (event) => {
+commentFormSubmit = async (event) => {
   event.preventDefault();
   const id = document.querySelector("#id").value;
   const body = document.querySelector("#comment-body").value.trim();
+
+  const response = await fetch("/api/comment", {
+    method: "POST",
+    body: JSON.stringify({ id, body }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
 };
+
+document
+  .querySelector("newComment")
+  .addEventListener("submit", commentFormSubmit);
